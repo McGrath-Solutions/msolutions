dcd7b86d1264ca8bb0d19bd20eef3c85c59bc91c
Michael McGrath <mmcgrath@mcgrathsolutions.org>
2014-10-13 11:02:53 -0400

    Update our-mission.php
    updated mission and vision statements

--- application/views/pages/about-us/our-mission.php
+++ application/views/pages/about-us/our-mission.php
@@ -1,10 +1,8 @@
 <h1>Our Mission</h1>
-<p>Our mission is to provide remote work experience to people with disabilities while providing low cost information technology services to 
-    nonprofit organizations and small businesses and to provide research and development of accessibility products.</p>
+<p>Our mission is to increase the employability of individuals with disabilities interested in science, technology, engineering, and mathematics (STEM) fields through internships and community outreach.</p>
 
 <h1>Our Vision</h1>
-<p>Our vision is that through the proper training, education, and work experience there will be an increase in the number of people with 
-    disabilities working in science, technology, engineering, and mathematics (STEM) positions.</p>
+<p>Our vision is to increase employment for individuals with disabilities.</p>
 
 <h1>Our Values</h1>
 <p>McGrath Solutions operates on the fact that anyone with a disability who wants to work can work. We never turn someone away because they 
